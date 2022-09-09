e3cc25b3bac5b41c60fd545ee8ce16aa33854309
benpkxd <113038163+benpkxd@users.noreply.github.com>
2022-09-09 23:32:07 +0800

    Update code_app01.py

--- code_app01.py
+++ code_app01.py
@@ -1,1 +1,31 @@
+import streamlit as st
+import panda as pd
+import numpy as np
+
+st.title("🏠 Home")                                                                                              
+
+st.header("👨‍🔬⚗️ Research Project 🔬")
+
+                                                                                              # get data in to variable
+df = pd.dataframe({
+'ID': [1,2,3,4],
+'Project' : ['PCR 自配','PCR 鉴定','益生菌','养殖技术']
+'Type' : ['PCR','PCR','PRO','Other']
+'Rate' : [5,3,4,4]
+})
+
+st.write(df)
+
+st.subheader("")
+
+
+st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+
+                                                                                              # make layout (sidebar)
+st.sidebar.title("Working in Aqua Business")
+
+
+
+animate_1 = st.balloon()
+animate_2 = st.snow()
 
