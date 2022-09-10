678dcc179ad919013eb25f368fea4e81028d35f0
benpkxd <113038163+benpkxd@users.noreply.github.com>
2022-09-10 09:26:05 +0800

    Update code_app01.py

--- code_app01.py
+++ code_app01.py
@@ -6,11 +6,11 @@ st.title("🏠 Home")
 
 st.header("👨‍🔬⚗️ Research Project 🔬")
 
-# get data
+# get data in to variable
 df = pd.DataFrame({
 'ID': [1,2,3,4],
-'Project' : ['PCR 自配','PCR 鉴定','益生菌','养殖技术']
-'Type' : ['PCR','PCR','PRO','Other']
+'Project' : ['PCR 自配','PCR 鉴定','益生菌','养殖技术'],
+'Type' : ['PCR','PCR','PRO','Other'],
 'Rate' : [5,3,4,4]
 })
 
@@ -21,21 +21,19 @@ st.subheader("")
 
 st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
 
-# make layout (sidebar)
+ # make layout (sidebar)
 st.sidebar.title("Working in Aqua Business")
 
+ # For funny widget
 
-# For funny widget
-animate_1 = st.balloon()
-animate_2 = st.snow()
 
 option_1 = st.selectbox(
      'Which type you need to celebrate?',
      ('Balloon', 'snow'))
 
 if option_1 == 'Balloon':
-  animate_1
+  st.balloons()
 elif option_1 == 'snow':
-  animate_2
+  st.snow()
   
 
