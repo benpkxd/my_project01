77e517f34a99ea3d85893d9d08c525ccc21f5f6d
benpkxd <113038163+benpkxd@users.noreply.github.com>
2022-09-18 22:04:02 +0800

    Update code_app01.py

--- code_app01.py
+++ code_app01.py
@@ -7,7 +7,7 @@ conn = connect()
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
+# @st.cache(ttl=600) if you need to delay to update
 def run_query(query):
     rows = conn.execute(query, headers=1)
     rows = rows.fetchall()
@@ -18,4 +18,4 @@ rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 # Print results.
 for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
+    st.write(f"{row.animal} has a {row.leg} legs")
